ca48521a4d419b00cfbdab2b8a54af5fe09685e5
Jonathan Roebuck <jonathan.roebuck@bbc.co.uk>
2021-04-21 11:48:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import PlaceholderText from './PlaceholderText';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="wrapper">
+        <PlaceholderText />
+        <div className="ad"></div>
+        <PlaceholderText />
+        <div className="ad"></div>
+        <PlaceholderText />
+
+        <div className="ad"></div>
+        <div className="ad"></div>
+      </div>
     </div>
   );
 }
---
+++ src/PlaceholderText.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+const PlaceholderText = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius
+        dolor ultricies, porttitor tortor volutpat, blandit nibh. Aliquam
+        iaculis risus risus, sed mattis neque fringilla sit amet. Morbi ac
+        condimentum ligula. Proin luctus tortor vitae posuere pretium. Praesent
+        quis mi id erat porta semper. Donec sem nisl, blandit sed euismod
+        commodo, convallis id ante. Pellentesque laoreet nisl eget massa
+        dignissim, non rhoncus lacus accumsan. Aliquam ornare eleifend dui, eget
+        consequat neque pretium non. Duis leo lorem, tempor eu dapibus quis,
+        ullamcorper a neque. Nunc tempus laoreet diam sit amet convallis.
+        Integer porttitor odio erat, quis posuere odio ultricies a. Curabitur a
+        nisl arcu. Aliquam ut congue lectus. Aenean sit amet nisi pulvinar,
+        feugiat libero a, dignissim odio. Pellentesque tincidunt sit amet nunc a
+        vehicula. Praesent placerat at diam in bibendum.
+      </p>
+
+      <p>
+        Morbi at lectus non dui ultrices tempus in ut risus. Aliquam iaculis
+        enim eget urna dapibus, non eleifend sem consectetur. Maecenas in quam
+        et magna gravida interdum. Aliquam hendrerit nibh at mollis euismod.
+        Curabitur malesuada nec velit id congue. Morbi elementum laoreet quam et
+        convallis. Morbi a dolor rhoncus, dignissim mauris et, pulvinar magna.
+        Duis id orci tortor. Sed quis faucibus lectus. Sed tempor tincidunt
+        finibus. Ut elementum diam non dui sagittis tincidunt. Suspendisse
+        potenti. Phasellus aliquam et eros vitae congue.
+      </p>
+
+      <p>
+        Fusce pretium lacus sem, id malesuada eros pellentesque ut. Donec luctus
+        rutrum metus id dapibus. Nulla pharetra, neque eu ullamcorper auctor,
+        nulla orci tempor velit, ac vestibulum massa ex quis leo. Nulla lobortis
+        in quam et rutrum. Vivamus eget commodo mi. Proin ornare aliquet dui non
+        iaculis. Proin vitae elit viverra, ornare leo a, molestie magna. Fusce
+        semper urna ut mauris tempus tempor. Nam consectetur ex mauris. Vivamus
+        ex ante, condimentum vel suscipit et, efficitur quis erat. Sed nec porta
+        orci, sed ornare orci. Aliquam sit amet mattis mauris. Aliquam nulla
+        diam, scelerisque sed lorem at, vestibulum blandit odio. Aliquam
+        placerat lacus metus, et pretium elit molestie in. Aliquam ac lobortis
+        urna. Proin elementum congue congue.
+      </p>
+
+      <p>
+        Curabitur eleifend nisi ante. Donec tempus felis et rhoncus tincidunt.
+        Phasellus sodales lectus nibh, in lacinia nulla pulvinar eu. Fusce a
+        luctus leo. Suspendisse congue posuere odio, quis bibendum felis.
+        Praesent rhoncus libero in mollis scelerisque. Suspendisse semper diam
+        at eros hendrerit vestibulum. Nunc ornare tempus consectetur. Etiam sit
+        amet tincidunt nisi. Vestibulum bibendum viverra volutpat. Donec
+        posuere, leo vitae ultricies dignissim, lorem arcu luctus lacus, mattis
+        hendrerit turpis felis ut arcu. Quisque semper mollis venenatis. Morbi
+        molestie metus sapien, quis maximus urna dapibus nec. Vestibulum
+        condimentum velit et sem ullamcorper, a tristique erat dapibus.
+      </p>
+    </>
+  );
+};
+
+export default PlaceholderText;
